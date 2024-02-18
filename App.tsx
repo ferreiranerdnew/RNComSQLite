04690ad1c49c556b841afc2536f839f53ffe2468
Renato Ferreira <91612020+ferreiranerdnew@users.noreply.github.com>
2024-02-18 15:19:38 -0300

    1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, StatusBar, TextInput, Button } from 'react-native';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+const App = () => {
+  const [category_, setCategory_] = useState("");
+const addCategory_ = () => {
+  
+};
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+      <StatusBar backgroundColor="orange" />
+      <TextInput
+        placeholder="Enter category"
+        value={category_}
+        onChangeText={setCategory_}
+        style={{ marginHorizontal: 8}}
+      />
+      <Button title="Submit" onPress={addCategory_}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
